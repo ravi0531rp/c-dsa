4c8361004eba2fe449d5bb0ce87182e3c8f5aecd
RAVI PRAKASH <ravi.0531.rp@gmail.com>
2025-02-02 00:12:49 +0530

    Create k-bin-tree-lca.py

---
+++ c-prep/files/g-trees/k-bin-tree-lca.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+
+        if root == p or root == q:
+            return root
+
+        left_lca = self.lowestCommonAncestor(root.left, p, q)
+        right_lca = self.lowestCommonAncestor(root.right, p, q)
+
+        if left_lca and right_lca:
+            return root
+        elif left_lca:
+            return left_lca
+        else:
+            return right_lca
+        
