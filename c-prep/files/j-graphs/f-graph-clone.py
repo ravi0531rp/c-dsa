e63b10b1539292d1a01e6567e766cc2011995305
RAVI PRAKASH <ravi.0531.rp@gmail.com>
2025-06-26 01:26:53 +0530

    Update f-graph-clone.py

--- c-prep/files/j-graphs/f-graph-clone.py
+++ c-prep/files/j-graphs/f-graph-clone.py
@@ -14,34 +14,32 @@ class Node {
 
 """
 
-# Definition for a Node.
+from collections import deque
+from typing import Optional
+
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
-from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
 
         old_to_new = {}
-        stack = [node]
-        seen = set()
-        seen.add(node)
+        queue = deque([node])
+        old_to_new[node] = Node(node.val)
 
-        while stack:
-            curr = stack.pop()
-            if curr not in old_to_new:
-                old_to_new[curr] = Node(val=curr.val)
+        while queue:
+            curr = queue.popleft()
+
             for neighbor in curr.neighbors:
-                if neighbor not in seen:
-                    seen.add(neighbor)
-                    stack.append(neighbor)
-
-        for old_node, new_node in old_to_new.items():
-            new_node.neighbors = [old_to_new[neighbor] for neighbor in old_node.neighbors]
+                if neighbor not in old_to_new:
+                    old_to_new[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                old_to_new[curr].neighbors.append(old_to_new[neighbor])
 
         return old_to_new[node]
+
         
